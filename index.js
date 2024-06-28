1275a64e8a3a7ac69efd44078624c38c4776dfab
Vivien Phang <101629147+vivienphang@users.noreply.github.com>
2024-06-28 14:33:25 +0800

    feat: added sort, groupByUserId and clear functionalities

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-// Global variables
+// GLOBAL
 let posts = [];
+let isSortAscending = false;
 
 window.onload = () => {
+  const loadingIndicator = document.getElementById("loadingIndicator");
+  loadingIndicator.style.display = "block";
   fetch("https://jsonplaceholder.typicode.com/posts")
     .then((response) => response.json())
     .then((data) => {
       posts = data;
       displayPosts(posts);
+      loadingIndicator.style.display = "none";
     })
     .catch((err) => {
       console.error("Error fetching data:", err);
+      loadingIndicator.innerHTML = "Failed to load data";
     });
 };
 
@@ -29,4 +34,46 @@ function displayPosts(posts) {
           )
           .join("")
       : "<div class='no-posts'>No posts available</div>";
+}
+
+// SORT AND FILTER FUNCTIONS
+function sortPosts() {
+  if (!posts || !posts.length) {
+    console.error("Posts are not loaded or are empty");
+    return;
+  }
+
+  if (isSortAscending) {
+    posts.sort((a, b) => a.title.localeCompare(b.title));
+  } else {
+    posts.sort((a, b) => b.title.localeCompare(a.title));
+  }
+  isSortAscending = !isSortAscending;
+  displayPosts(posts);
+}
+
+function groupByUserId() {
+  const userId = parseInt(document.getElementById("userId").value, 10);
+  if (!userId) {
+    alert("Please enter a valid User ID.");
+    return;
+  }
+
+  const filteredPosts = posts.filter((post) => post.userId === userId);
+  if (!filteredPosts.length) {
+    alert("No posts found for this User ID.");
+    return;
+  }
+  displayPosts(filteredPosts);
+}
+
+function clearFilter() {
+  // Reset global variables
+  currentPagePosts = [];
+  currentPage = 1;
+  isSortAscending = false;
+
+  // Revert to default
+  displayPosts(posts);
+  document.getElementById("userId").value = "";
 }
