1765ce8a40c7b69de8a6694e48dabcaae1bc5fc6
Vivien Phang <101629147+vivienphang@users.noreply.github.com>
2024-06-28 14:16:35 +0800

    feat: fetched API data and styling for each cards

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Test");
+// Global variables
+let posts = [];
+
+window.onload = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((data) => {
+      posts = data;
+      displayPosts(posts);
+    })
+    .catch((err) => {
+      console.error("Error fetching data:", err);
+    });
+};
+
+function displayPosts(posts) {
+  const container = document.querySelector(".posts-container");
+  container.innerHTML =
+    posts.length > 0
+      ? posts
+          .map(
+            (post) => `
+            <div class="card" key="${post.id}">
+              <div class="content">
+              <h3>${post.title}</h3>
+              <p>${post.body}</p>
+              </div>
+            </div>`
+          )
+          .join("")
+      : "<div class='no-posts'>No posts available</div>";
+}
